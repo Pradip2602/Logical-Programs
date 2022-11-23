441ed75cfbcde5f0683dbfb0a137c7e6fae64d82
Pradip Ravindra Chavan <98585259+Pradip2602@users.noreply.github.com>
2022-11-23 13:13:55 +0530

    Update CalculateFactorialOfNumber.java

--- CalculateFactorialOfNumber.java
+++ CalculateFactorialOfNumber.java
@@ -3,7 +3,7 @@ class Test
 {
 		public static void main(String [] args)
 		{
-				// Reverse the number 
+				// Factorial of number 
 				Scanner sc = new Scanner(System.in);
 				System.out.println("Enter the number : ");
 				int num = sc.nextInt();
