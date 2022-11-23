30e99bfb25cc5ec282067d4d564d7bad004e578d
Pradip Ravindra Chavan <98585259+Pradip2602@users.noreply.github.com>
2022-11-23 12:41:11 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Test
+{
+		public static void main(String [] args)
+		{
+				// find the number is odd or even
+
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Enter the number");
+				int num = sc.nextInt();
+				
+				int temp = 0;
+				if(num!=0)
+				{
+						for(int i=2; i<num-1; i++)
+						{
+							if(num%i==0)
+							{
+								temp++;
+							}
+						}
+					if(temp == 0)
+					{
+						System.out.println(num +" is prime number");
+					}
+					else
+					{
+						System.out.println(num +" is not prime");
+					}
+				}
+				
+		}
+
+}
